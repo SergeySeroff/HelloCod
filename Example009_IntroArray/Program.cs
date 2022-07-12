07576725fd89385274bb343d260d3e5d3270dd72
SergeySeroff <harek20@mail.ru>
2022-07-12 16:35:38 +0300

    add Example 009 IntroArray

--- Example009_IntroArray/Program.cs
+++ Example009_IntroArray/Program.cs
@@ -4,8 +4,12 @@
     if (arg2 > result) result = arg2;
     if (arg3 > result) result = arg3;
     return result;
-}//           0   1  2  3  4  5  6  7  8
+}//              0   1   2   3   4   5   6   7   8
 int[] array = { 11, 12, 21, 41, 15, 61, 17, 18, 19 };
-array[0] = 12;
-Console.WriteLine(array[0]);
+// array[0] = 12;
+// Console.WriteLine(array[4]);
 
+int result = Max(Max(array[0],array[1],array[2]),
+                 Max(array[3],array[4],array[5]),
+                 Max(array[6],array[7],array[8]));
+Console.WriteLine(result);
