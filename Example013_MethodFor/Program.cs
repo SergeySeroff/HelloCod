3c7555aaeb3aa36cc35bb9eefc91c9e1a8862474
SergeySeroff <harek20@mail.ru>
2022-07-25 15:05:46 +0300

    add Example 13 Method For ready

---
+++ Example013_MethodFor/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+for (int i = 2; i <= 9; i++)
+{
+    for (int j = 2; j <= 9; j++)
+    {
+        Console.WriteLine($"{i} * {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
+*/
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+            + "ежеле бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty"
+//             012
+// s[3] // = r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'с', 'С');
+Console.WriteLine(newText);
